96988c75305b85675861a642e20269db1104b7c7
rjtnchn <24riexd@gmail.com>
2024-03-03 23:41:26 +0800

    update

--- main/admin.py
+++ main/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Poem
+from .models import Poem, Question
 
 admin.site.register(Poem)
+admin.site.register(Question)
+
 
 # Register your models here.
 
--- main/models.py
+++ main/models.py
@@ -10,4 +10,5 @@ class Question(models.Model):
     poem = models.ForeignKey(Poem, on_delete=models.CASCADE)
     difficulty = models.CharField(max_length=10) #Easy, Normal, Hard
     question_text = models.TextField()
+    choices = models.CharField(max_length=5)
     correct_answer = models.CharField(max_length=100)
