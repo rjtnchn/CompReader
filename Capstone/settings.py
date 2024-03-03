7a57beba7c2cc921f4219d6d8a4cf89ac0cc29ba
rjtnchn <24riexd@gmail.com>
2024-03-03 21:49:46 +0800

    update files

--- Capstone/settings.py
+++ Capstone/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'quiz.apps.QuizConfig',
+    'main.apps.MainConfig',
 
 
 ]
@@ -129,5 +129,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_REDIRECT_URL = '/home'
-LOGOUT_REDIRECT_URL= '/login'
+LOGIN_URL = '/login/'
--- main/urls.py
+++ main/urls.py
@@ -5,4 +5,9 @@ urlpatterns = [
     path('', views.home, name='home' ),
     path('home/', views.home, name='home' ),
     path('sign-up/', views.sign_up, name='sign_up' ),
+    path('quiz/', views.quiz, name='quiz'),
+    path('poem/<int:poem_id/', views.difficulty, name='difficulty'),
+    path('submit_answers/', views.submit_answers, name='submit_answers'),
+    path('logout/', views.user_logout, name='user_logout'),
+    path('register/', views.register, name='register'),
 ]
