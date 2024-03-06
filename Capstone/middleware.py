7c9480c87cf4d1927d65c373e83e3c9de581712f
rjtnchn <24riexd@gmail.com>
2024-03-07 04:38:47 +0800

    commit

--- Capstone/middleware.py
+++ Capstone/middleware.py
@@ -1,18 +1,18 @@
-# from django.shortcuts import redirect
-# from django.urls import reverse
+from django.shortcuts import redirect
+from django.urls import reverse
 
-# class ReadingTimeMiddleware:
-#     def __init__(self, get_response):
-#         self.get_response = get_response
+class ReadingTimeMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
 
-#     def __call__(self, request):
-#         if request.path.startswith('/questions/'):
-#             # Check if the user has already read the poem for 10 minutes
-#             # If not, redirect them back to the poem view
-#             if not request.session.get('poem_read'):
-#                 # Redirect to the poem view with the appropriate poem_id
-#                 poem_id = request.session.get('poem_id')
-#                 return redirect(reverse('display_poem', kwargs={'difficulty_id': poem_id}))
+    def __call__(self, request):
+        if request.path.startswith('/questions/'):
+            # Check if the user has already read the poem for 10 minutes
+            # If not, redirect them back to the poem view
+            if not request.session.get('poem_read'):
+                # Redirect to the poem view with the appropriate poem_id
+                poem_id = request.session.get('poem_id')
+                return redirect(reverse('display_poem', kwargs={'difficulty_id': poem_id}))
 
-#         response = self.get_response(request)
-#         return response
+        response = self.get_response(request)
+        return response
